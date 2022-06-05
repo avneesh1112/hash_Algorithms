5ed29f6a76eebd9551b0f2c65e2533da60784938
Avneesh shukla <84228749+avneesh1112@users.noreply.github.com>
2022-06-05 12:43:11 +0530

    Create sha256.h

---
+++ sha256.h
@@ -0,0 +1,19 @@
+#ifndef SHA256_H
+#define SHA256_H
+
+#define VERBOSE		1
+#define NOT_VERBOSE	0
+
+/******************************************************************************/
+//Return 32 bytes digest of Data array on success. Return NULL if fail.
+//VerboseStatus = SHA256_VERBOSE --> Will print progress
+//VerboseStatus = SHA256_NOT_VERBOSE --> Will not print progress
+uint8_t *sha256_data(uint8_t *Data, uint64_t DataSizeByte, uint8_t VerboseStatus);
+
+//Return 32 bytes digest of file on success. Return NULL if fail.
+//VerboseStatus = SHA256_VERBOSE --> Will print progress
+//VerboseStatus = SHA256_NOT_VERBOSE --> Will not print progress
+uint8_t *sha256_file(const char *Filename, uint8_t VerboseStatus);
+
+
+#endif
